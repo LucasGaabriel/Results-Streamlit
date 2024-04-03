f9274e0971a17f6c6a44262ae36e1250aa64d8b6
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2024-04-03 16:26:20 -0300

    Some changes. Biased/Unbiased and Metrics

--- main.py
+++ main.py
@@ -20,11 +20,6 @@ datasets = (
     "XJTU",
 )
 
-classifiers = (
-    "Random Forest",
-    "Neural Network: xresnet",
-)
-
 option_datasets = st.selectbox(
     "Which dataset would you like to show?",
     datasets,
@@ -32,55 +27,43 @@ option_datasets = st.selectbox(
     placeholder="Select a dataset."
 )
 
-option_classifier = st.selectbox(
-    "Which classifier would you like to show?",
-    classifiers,
-    index=None,
-    placeholder="Select a classifier."
-)
-
-option_interpol = st.radio(
-    "Must the samples be interpolated?",
-    key='interpol',
-    options=["Default", "Interpolation"],
-    index=None,
-    horizontal=True
-)
-
-# disable_option_bias = True if option_interpol == "Interpolation" else False
-
 option_bias = st.radio(
     "Do you want to show the Biased or Unbiased results?",
-    key='bias',
-    # disabled=disable_option_bias,
     options=["Biased", "Unbiased"],
     index=None,
     horizontal=True
 )
 
-if option_datasets and option_classifier and option_bias:
+option_metric = st.radio(
+    "Which metric do you want to show?",
+    options=["Accuracy", "Balanced Accuracy", "F1 Macro"],
+    index=None,
+    horizontal=True
+)
 
-    if "Random Forest" in option_classifier:
-            json_path = ("./results/ml/" + option_bias.lower() + "/" + option_datasets + ".json")
-            result = json.load(open(json_path))
-            data = {'test_accuracy': result['accuracy'],
-                    'test_balanced_accuracy': result['balanced accuracy'],
-                    'test_f1_macro': result['macro avg']['f1-score']}
-            df = pd.DataFrame(data, index=[0])
+if option_datasets and option_bias and option_metric:
 
-    if "Neural Network" in option_classifier:
-        if "Interpolation" in option_interpol:
-            csv_path = ("./results/deep/" + option_interpol.lower() + "/"
-                        + "/results_" + option_datasets.lower() + ".csv")
-        else:
-            csv_path = ("./results/deep/" + option_interpol.lower() + "/" + option_bias.lower()
-                        + "/results_" + option_datasets.lower() + ".csv")
+    df = pd.DataFrame()
+    metric = "test_" + option_metric.lower().replace(' ', '_')
 
-        df = pd.read_csv(csv_path)
-        if 'fold' in df.columns.tolist():
-            df = df.drop(['fold'], axis=1)
-        df = df.drop(['fit_time', 'score_time'], axis=1)
+    # Machine Learning
+    json_path = ("./results/ml/" + option_bias.lower() + "/" + option_datasets + ".json")
+    result = json.load(open(json_path))
+    data = {'test_accuracy': result['accuracy'],
+            'test_balanced_accuracy': result['balanced accuracy'],
+            'test_f1_macro': result['macro avg']['f1-score']}
+    df.insert(0, "ml", pd.Series(data[metric]))
+
+    # Deep Learning without interpolation
+    csv_path = ("./results/deep/default/" + option_bias.lower() + "/results_" +
+                option_datasets.lower() + ".csv")
+    df_dl = pd.read_csv(csv_path)
+    df.insert(1, "dl_default", pd.Series(df_dl[metric].mean()))
+    # df = pd.concat([df, pd.Series(df_dl[metric].mean())], ignore_index=True)
+
+    # Deep Learning with interpolation
+    csv_path = ("./results/deep/interpolation/results_" + option_datasets.lower() + ".csv")
+    df_dl = pd.read_csv(csv_path)
+    df.insert(2, "dl_interpolation", pd.Series(df_dl[metric].mean()))
 
     st.bar_chart(df)
-
-
