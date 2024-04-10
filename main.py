fe7b7784416b4e06d58bd39409a45f8bdc28d6f2
Lucas Gabriel <lucasgabriiel.oliveira@gmail.com>
2024-04-10 16:58:29 -0300

    Add 1NN Results and include Biased Mirrored Option

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ import pandas as pd
 import streamlit as st
 
 st.write("""
-# Experiment Results
+# Experiments Results
 """)
 
 datasets = (
     "CWRU 12000",
     "CWRU 48000",
-    "EAS",
+    # "EAS",
     "IMS",
-    "MAFAULDA",
+    # "MAFAULDA",
     "MFPT 48828",
     "MFPT 97656",
     "PU",
     "UOC",
-    "XJTU",
+    # "XJTU",
 )
 
 option_datasets = st.selectbox(
@@ -29,7 +29,7 @@ option_datasets = st.selectbox(
 
 option_bias = st.radio(
     "Do you want to show the Biased or Unbiased results?",
-    options=["Biased", "Unbiased"],
+    options=["Biased Usual", "Biased Mirrored", "Unbiased"],
     index=None,
     horizontal=True
 )
@@ -41,31 +41,49 @@ option_metric = st.radio(
     horizontal=True
 )
 
-if option_datasets and option_bias and option_metric:
 
+def json_data(json, metric):
+    if metric == 'test_accuracy':
+        return json['accuracy']
+    elif metric == 'test_balanced_accuracy':
+        return json['balanced accuracy']
+    elif metric == 'test_f1_macro':
+        return json['macro avg']['f1-score']
+
+
+if option_datasets and option_bias and option_metric:
     dataset = option_datasets.lower().replace(' ', '_')
-    bias = option_bias.lower()
+    bias = option_bias.lower().replace(' ', '_')
     metric = "test_" + option_metric.lower().replace(' ', '_')
+
+    indexes = []
     results = []
 
-    # Machine Learning
-    json_path = ("./results/ml/" + bias + "/" + dataset + ".json")
+    # 1NN
+    json_path = ("./results/ml/1nn/" + bias + "/" + dataset + ".json")
     result = json.load(open(json_path))
-    data = {'test_accuracy': result['accuracy'],
-            'test_balanced_accuracy': result['balanced accuracy'],
-            'test_f1_macro': result['macro avg']['f1-score']}
-    results.append(data[metric])
+    data = json_data(result, metric)
+    indexes.append("(1) 1NN")
+    results.append(data)
+
+    # Random Forest
+    json_path = ("./results/ml/randomforest/" + bias + "/" + dataset + ".json")
+    result = json.load(open(json_path))
+    data = json_data(result, metric)
+    indexes.append("(2) RF")
+    results.append(data)
 
     # Deep Learning without interpolation
     csv_path = ("./results/deep/default/" + bias + "/results_" + dataset + ".csv")
     df_dl = pd.read_csv(csv_path)
+    indexes.append("(3) CNN Default")
     results.append(df_dl[metric].mean())
 
     # Deep Learning with interpolation
     csv_path = ("./results/deep/interpolation/results_" + dataset + ".csv")
     df_dl = pd.read_csv(csv_path)
+    indexes.append("(4) CNN Interpolated")
     results.append(df_dl[metric].mean())
 
-    index = ["(1) RF", "(2) CNN Default", "(3) CNN Interpolated"]
-    df = pd.DataFrame(results, index=index, columns=["Results"])
+    df = pd.DataFrame(data=results, index=indexes, columns=["Results"])
     st.bar_chart(df)
